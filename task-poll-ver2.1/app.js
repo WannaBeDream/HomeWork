e922411e9f0f8b897f2560ed83902434f7b9c211
Alex <alexxldub@gmail.com>
2019-03-11 16:33:34 +0200

    new ver 2.1

---
+++ task-poll-ver2.1/app.js
@@ -0,0 +1,52 @@
+let points = 0 ;
+let dataOfPoll = [
+ { question: 'Сколько будет 2+2?',
+   answer: 4, 
+   type: 'prompt' 
+ },
+ { question: 'Солнце встает на востоке?',
+   answer: true,
+   type: 'confirm' 
+ },
+ { question: 'Сколько будет 5 << 2 ?',
+   answer: 20,
+   type: 'prompt'
+ }
+ ] ;
+
+
+
+function askQuestion(dataOfPoll) {
+    let askUser;
+    switch(dataOfPoll.type) {
+        case  'prompt' :
+        askUser = prompt(dataOfPoll.question, '') ;
+        if(!validation(askUser)) {
+            alert('Wrong,try again plz or suffer user');
+            askQuestion(dataOfPoll);
+    }   break;  
+    
+    case 'confirm' : 
+        askUser = confirm(dataOfPoll.question);
+        break;
+        default: alert('type is invalid');
+    }
+
+    return askUser == dataOfPoll.answer;
+}
+
+
+function validation(value) {
+    // return !isNaN(value) || !value;
+    return !(isNaN(value) || value === '' || value === null) ;
+
+}
+
+
+
+for (let i = 0; i < dataOfPoll.length; i++) {
+    let isAnswerCorrect = askQuestion(dataOfPoll[i]) ;
+    points = isAnswerCorrect ? 10 : 0 ;
+}
+
+alert('All points: ', points) ;
