ada8516c05b08f4a1eb2a117a7d7b19be37daf10
Alex <alexxldub@gmail.com>
2019-03-24 13:18:27 +0200

    ver1.1

--- work with DOM/app.js
+++ work with DOM/app.js
@@ -4,7 +4,7 @@ headLine[0].textContent = "Hello, " + getName('Как вас зовут?');
 
 function getName(questionFirst) {
 let name = prompt(questionFirst,'')
- if (validateName(name)) {
+ if (!validateName(name)) {
     alert('plz try again');
     return getName(questionFirst);
  } else {
@@ -12,13 +12,9 @@ let name = prompt(questionFirst,'')
  }
 }
 
-
 function validateName(value) {
-    return (!isNaN(value) || value === '' || value === null ) ;
-
+    return !(!isNaN(value) || value === '' || value === null ) ;
 }
-
-
 
 ///////////////
 
@@ -26,16 +22,13 @@ const newElemUl = document.createElement('ul');
 document.body.appendChild(newElemUl);
 
 
-
-
 function validateNumber(value) {
-    return (isNaN(value) ||  value === null ||  value === '' || value > 100 ||  value < 1);
+    return !(!isNaN(value) ||  value === null ||  value === '' || value > 100 ||  value < 1);
 }
 
 
-
 function getLiCounts(questionSecond) {
-    let numbers = +prompt(questionSecond,'');
+    let numbers = prompt(questionSecond,'');
     if(validateNumber(numbers)){
          return getLiCounts(questionSecond)
         } else {
@@ -46,12 +39,10 @@ function getLiCounts(questionSecond) {
     
 function textCreate(value) {
     let str = '';
-    
     for(let i = 0; i < value; i++) {
          str += '<li>' + (i + 1) + '</li>';
-         
     }
-        return str;
+     return str;
     }
     
     const liCounts = +getLiCounts('Введите количество заголовков');
