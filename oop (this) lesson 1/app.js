5cfc6a4dec7ead781e17884d495c6f4d9ae40a9e
Alex <alexxldub@gmail.com>
2019-04-06 18:04:13 +0300

    ver 1.1

--- oop (this) lesson 1/app.js
+++ oop (this) lesson 1/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function Student(name,marks){
+function Student(name, marks) {
   this.name = name;
   this.marks = marks;
   this.averageMark = averageMark;
@@ -9,51 +9,44 @@ function Student(name,marks){
 
 }
 
-function averageMark(){
-    
-  const sumMarks = this.marks.reduce( (previousMark, currentMark) => 
-  previousMark + currentMark );
-    return (sumMarks / this.marks.length).toFixed(3);
+function averageMark() {
+  const sumMarks = this.marks.reduce((previousMark, currentMark) =>
+    previousMark + currentMark);
+
+  return (sumMarks / this.marks.length).toFixed(3);
 
 }
 
-function woksDone(){
+function woksDone() {
+  return this.marks.filter((number) => { return number > 0 }).length;
 
-    return this.marks.filter((number) => 
-    { return number > 0}).length;
-      
 }
 
-function addMark(value){
-
+function addMark(value) {
   return this.marks.push(value);
 
 }
 
-function averageMarkGroup(){
-
-  const  countsMarksGroup = students.reduce ((previousValue, currentValue) =>
-  previousValue + +currentValue.averageMark(),0);
+function averageMarkGroup() {
+  const countsMarksGroup = students.reduce((previousValue, currentValue) =>
+    previousValue + +currentValue.averageMark(), 0);
   return (countsMarksGroup / students.length).toFixed(3);
-   
+
 }
 
-function completePercent(){
+function completePercent() {
+  const sumWoksOfGroup = students.reduce((previousValue, currentValue) => { return previousValue + currentValue.woksDone() }, 0);
+  const countsWoks = students.reduce((previousValue, currentValue) => { return previousValue + currentValue.marks.length }, 0);
 
-  const sumWoksOfGroup = students.reduce( (previousValue, currentValue) => 
-    {return previousValue + currentValue.woksDone()}, 0);
-  const countsWoks = students.reduce( (previousValue, currentValue) => 
-    {return previousValue + currentValue.marks.length}, 0);
-
-        return (sumWoksOfGroup / countsWoks) * 100 + ' %';
+  return (sumWoksOfGroup / countsWoks) * 100 + ' %';
 
 }
 
 
 
-const students = [ 
-    new Student('Student 1', [10,10]), // имя оценки
-    new Student('Student 12', [10,10])
+const students = [
+  new Student('Student 1', [10, 10]), // имя оценки
+  new Student('Student 12', [10, 10])
 ];
 
 // averageMark() - возвращает среднюю оценку
@@ -62,9 +55,9 @@ const students = [
 // averageMark() - которая возвращает среднюю оценку по группе
 // coьpletePercent() - процент сданных работ по группе
 
- console.log(students[0].averageMark());
- console.log(students[0].addMark(10));
- console.log(students[0].woksDone());
- console.log(students[0].averageMark());
- console.log(averageMarkGroup());
- console.log(completePercent());
+console.log(students[0].averageMark());
+console.log(students[0].addMark(10));
+console.log(students[0].woksDone());
+console.log(students[0].averageMark());
+console.log(averageMarkGroup());
+console.log(completePercent());
