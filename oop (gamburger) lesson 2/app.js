e745683451caeb8232d020cd59ee158f08e61257
Alex <alexxldub@gmail.com>
2019-04-10 19:07:21 +0300

    ver.1.1

--- oop (gamburger) lesson 2/app.js
+++ oop (gamburger) lesson 2/app.js
@@ -1,36 +1,34 @@
 "use strict";
 
 function Hamburger(size, stuffing) {
-  this.privateSize = size;
-  this.privateStuffing = stuffing;
-  this.privateTopping = [];
+  this._Size = size;
+  this._Stuffing = stuffing;
+  this._Topping = [];
 
 }
 
 Hamburger.prototype.addTopping = function (topping) {
-  if (!this.privateTopping.includes(topping)) {
-    return this.privateTopping.push(topping);
+  if (!this._Topping.includes(topping)) {
+    return this._Topping.push(topping);
   }
 }
 
 Hamburger.prototype.calculateCalories = function () {
-  let allCalories = this.privateSize.calories +
-    this.privateStuffing.calories +
-    this.privateTopping.reduce((accum, currentValue) => accum + currentValue.calories, 0);
-  return allCalories;
+  return this._Size.calories +
+    this._Stuffing.calories +
+    this._Topping.reduce((accum, currentValue) => accum + currentValue.calories, 0);
 
 }
 
 
 Hamburger.prototype.calculatePrice = function () {
-  let allPrice = this.privateSize.price +
-    this.privateStuffing.price +
-    this.privateTopping.reduce((accum, currentValue) => accum + currentValue.price, 0);
-  return allPrice;
+  return this._Size.price +
+    this._Stuffing.price +
+    this._Topping.reduce((accum, currentValue) => accum + currentValue.price, 0);
 }
 
 Hamburger.prototype.getArrHumb = function () {   // добавить в сalc константы и вернуть массив + map(любой создающиий arr) -> + push -> + reduce
-  return [this.privateTopping, this.privateSize, this.privateStuffing]
+  return [this._Topping, this._Size, this._Stuffing]
 }
 
 Hamburger.SIZE_SMALL = { price: 50, calories: 20 }
