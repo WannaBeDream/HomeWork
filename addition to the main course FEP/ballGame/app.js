d699a0a507c0ba7b057366bc1cdf0ff328e11b28
Alex <alexxldub@gmail.com>
2019-04-14 00:35:47 +0300

    ver 1.0

---
+++ addition to the main course FEP/ballGame/app.js
@@ -0,0 +1,111 @@
+function BallGame(el, height, width){
+  this.field = { height, width };
+  this.game = {
+      direction: 'right',
+      speed: 10,
+      refreshDelay: 300,
+      state: 'paused'
+  }
+
+  this.init = function() {
+      el.classList.add('ball-game-field');
+      this.setHeight(height);
+      this.setWidth(width);
+      this.addBall();
+      this.start();
+      this.bindEventListeners();
+  }
+
+  this.bindEventListeners = function(){
+      document.body.addEventListener('keyup', (e) => this.onKeyboardEvent(e));
+  }
+
+  this.onKeyboardEvent = function(event){
+      switch(event.code){
+          case 'Space': this.toggleState(); break;
+          case 'ArrowUp': this.setDirection('up'); break;
+          case 'ArrowDown': this.setDirection('down'); break;
+          case 'ArrowLeft': this.setDirection('left'); break;
+          case 'ArrowRight': this.setDirection('right'); break;
+      }
+  }
+
+  this.setHeight = function(height){
+      this.field.height = height;
+      el.style.height = height + 'px';
+  }
+  this.setWidth = function(width){
+      this.field.width = width;
+      el.style.width = width + 'px';
+  }
+
+  this.setDirection = function(direction){
+      this.game.direction = direction;
+  }
+
+  this.addBall = function(){
+      this.ballEl = document.createElement('div');
+      this.ballEl.className = 'ball-game-ball';
+      el.appendChild(this.ballEl);
+  }
+
+  this.start = function(){
+      if (this.game.state === 'paused'){
+          this.game.state = 'playing';
+          this.tick();
+      }
+  }
+  this.pause = function(){
+      this.game.state = 'paused';
+  }
+
+  this.toggleState = function(){
+      if (this.game.state === 'paused') {
+          this.start();
+      } else {
+          this.pause();
+      }
+
+  }
+
+  this.tick = function(){
+      if (this.game.state === 'playing'){
+          const coordsOffset = this.getCoordsOffset();
+
+          this.moveBall(coordsOffset);
+
+          setTimeout(() => this.tick(), this.game.refreshDelay);
+      }
+  }
+
+  this.getCoordsOffset = function(){
+      switch (this.game.direction){
+          case 'right': return {top: 0, left: 1 * this.game.speed};
+          case 'left': return {top: 0, left: -1 * this.game.speed};
+          case 'up': return {top: -1 * this.game.speed, left: 0};
+          case 'down': return {top: 1 * this.game.speed, left: 0};
+          default: return {top: 0, left: 0};
+      }
+  }
+
+  this.moveBall = function(offset){
+      let newTop = this.ballEl.offsetTop + offset.top;
+      let newLeft = this.ballEl.offsetLeft + offset.left;
+
+      if (newTop >= this.field.height) newTop = 0;
+      if (newTop < 0 ) newTop = this.field.height + newTop;
+      if (newLeft >= this.field.width) newLeft = 0;
+      if (newLeft < 0 ) newLeft = this.field.width + newLeft;
+
+      this.ballEl.style.top = newTop + 'px';
+      this.ballEl.style.left = newLeft + 'px';
+  }
+}
+
+const game = new BallGame(
+                      document.getElementById('container'),
+                      400,
+                      400
+                  );
+
+game.init();
---
+++ oop (akardion event loop) lesson 3/app.js
@@ -0,0 +1,133 @@
+function Accordion(el, config) {
+    this.el = el;
+    this.config = config;
+}
+
+Accordion.prototype.init = function () {
+
+    Accordion.prototype.hiddenBody.call(this);
+    this.accordionHeader = this.el.getElementsByClassName('accordeon-heading');
+
+    for (var i = 0; i < this.accordionHeader.length; i++) {
+        this.accordionHeader[i].addEventListener('click', this.onBodyAccordion.bind(this));
+    }
+
+}
+Accordion.prototype.hiddenBody = function () {
+    this.accordionBody = this.el.getElementsByClassName('accordeon-body');
+    for (var i = 0; i < this.accordionBody.length; i++) {
+        this.accordionBody[i].hidden = true;
+        this.accordionBody[i].setAttribute('index', i);
+    }
+}
+
+Accordion.prototype.toСollapseOther = function (index) {
+
+    for (let i = 0; i < this.accordionBody.length; i++) {
+        if (i == index) continue;
+        this.accordionBody[i].hidden = true;
+
+    }
+}
+
+
+Accordion.prototype.open = function (index) {
+
+    this.accordionBody[index].hidden = false;
+}
+
+Accordion.prototype.close = function (index) {
+
+    this.accordionBody[index].hidden = true
+}
+
+Accordion.prototype.toggle = function (index) {
+    if (this.accordionBody[index].hidden == true) this.accordionBody[index].hidden = false
+    else this.accordionBody[index].hidden = true
+}
+
+
+Accordion.prototype.onBodyAccordion = function (event) {
+    let ElemClicked = event.target.nextElementSibling.getAttribute('index');
+
+    this.open(ElemClicked) ? this.close(ElemClicked) : this.open(ElemClicked);
+    if (!this.config.collapseOther) this.toСollapseOther(ElemClicked);
+
+
+}
+
+const accordion = new Accordion(
+    document.getElementById('container'),
+    { collapseOther: false }
+);
+
+
+accordion.init();
+
+// accordion.open(1);
+// accordion.close(0);
+// accordion.toggle(1);
+
+
+
+
+
+
+
+
+// Ниже второй вариант через классы CSS и el.style (доделать!)
+
+
+
+
+
+// function Accordeon(el, config){
+//     this.el = el;
+//     this.config = config;
+
+// }
+
+// Accordeon.prototype.init = function () { 
+
+//     this.bindEventListeners();
+
+// }
+
+// Accordeon.prototype.bindEventListeners = function() {
+//     this.accHeads = this.el.querySelectorAll('accordion-element accordeon-heading');
+//     for(let i = 0; i < this.accHeads.length; i++) {
+//         this.accHeads[i].addEventListener('click', (e) => this.openCurrAccordion.bind(this));
+//         console.log(this.accHeads[i]);
+//     }
+//     console.log(this.accHeads[0]);
+// }
+
+//     Accordeon.prototype.openCurrAccordion = function (event) {
+//         for(let i = 0; i < this.accHeads.length; i++) {
+//             this.parent = this.accHeads[i].parentElement;
+//             this.nextEl = this.accHeads[i].nextElementSibling;
+
+//             if (this === accHeads[i] && !parent.classList.contains('open')) {
+//                 this.parent.classList.add('open');
+//                 this.nextEl.style.maxHeight = article.scrollHeight + 'px';
+//             }
+//             else {
+//                 this.parent.classList.remove('open');
+//                 this.nextEL.style.maxHeight = '0px';
+//             }
+//         }
+//     }
+
+
+
+
+// const accordion = new Accordeon(
+//                         document.getElementById('container'),
+//                         {collapseOther: false}
+//                     );
+
+
+// accordion.init();
+// accordion.open(1);
+// accordion.close(0);
+// accordion.toggle(1);
