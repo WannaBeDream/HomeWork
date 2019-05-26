02a7cb1e717a31b7994180821ddf937379cb7a43
Alex <alexxldub@gmail.com>
2019-05-26 15:08:51 +0300

    change index.js and delete lodash

--- node-hw/calculator/index.js
+++ node-hw/calculator/index.js
@@ -1,11 +1,6 @@
-const add = require('./add');
-const div = require('./div');
-const mult = require('./mult');
-const sub = require('./sub');
-
 module.exports = {
-    add,
-    div,
-    mult,
-    sub
+    add: require('./add'),
+    div: require('./div'),
+    mult: require('./mult'),
+    sub: require('./sub')
 }
