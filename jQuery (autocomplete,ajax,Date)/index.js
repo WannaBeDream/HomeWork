abeebf300fd1035e1cdac95b9e6ebd54c8108398
Alex <alexxldub@gmail.com>
2019-05-29 16:56:48 +0300

    version 1.0

--- jQuery (autocomplete,ajax,Date)/index.js
+++ jQuery (autocomplete,ajax,Date)/index.js
@@ -1,5 +1,7 @@
 $(function () {
 
+  const URL_USERS_SEARCH = "https://api.github.com/search/users?q=";
+  const URL_USERS = "https://api.github.com/users/";
   const wrapperUi = $('.wrapper-ui');
   const userImg = $('.user-img');
   const userName = $('.user-name');
@@ -8,52 +10,43 @@ $(function () {
   const userFollowers = $('#user-followers');
   const userRegistration = $('#user-registration');
 
-
-  //Показать данные пользователя
-  function renderUserInfo(message) {
-    let date = new Date(message.created_at);
-    date = `${date.getDate()} - ${date.getMonth()} - ${date.getFullYear()}`;
-
-    wrapperUi.css("display", "flex");
-    userImg.attr("src", message.avatar_url);
-    userName.html(message.name);
-    userLogin.attr("href", message.html_url);
-    userLogin.html("(" + message.login + ")");
-    userRepositories.html(message.public_repos);
-    userFollowers.html(message.followers);
-    userRegistration.html(date);
-  }
-
-  //Сформировать список логинов
-  function getListOfLogins(data) {
-    let loginsArr = [];
-    for (let i = 0; i < data.items.length; i++) {
-      loginsArr.push(data.items[i].login);
-    }
-    return loginsArr;
-  }
-
-
   $("#select-user").autocomplete({
     source: function (request, response) {
       $.ajax({
-        url: "https://api.github.com/search/users?q=" + request.term,
-        type: "GET",
-        success: function (data) {
-          response(getListOfLogins(data));
-          ;
-        }
+        url: URL_USERS_SEARCH + request.term,
+        type: "GET"
+      }).done((data) => {                   //success: function
+        response(getListLogins(data));
       });
     },
     minLength: 2,
     select: function (event, ui) {
       $.ajax({
-        url: "https://api.github.com/users/" + ui.item.label,
-        type: "GET",
-        success: function (data) {
-          renderUserInfo(data);
-        }
+        url: URL_USERS + ui.item.label,
+        type: "GET"
+      }).done((data) => {               //success: function
+        renderUserInfo(data);
       });
     }
   });
+
+  function renderUserInfo(data) {
+    let date = new Date(data.created_at);
+    let mainDate = `${date.getDate()} - ${date.getMonth()} - ${date.getFullYear()}`;
+
+    wrapperUi.css("display", "flex");
+    userImg.attr("src", data.avatar_url);
+    userName.html(data.name);
+    userLogin.attr("href", data.html_url);
+    userLogin.html("(" + data.login + ")");
+    userRepositories.html(data.public_repos);
+    userFollowers.html(data.followers);
+    userRegistration.html(mainDate);
+  }
+
+
+  function getListLogins(data) {
+    return data.items.map((item) => item.login)
+  }
+
 });
