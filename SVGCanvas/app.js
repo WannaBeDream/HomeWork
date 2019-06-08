9d90b989aa6782d77bf215979329cb341ab4baca
Alex <alexxldub@gmail.com>
2019-06-08 19:50:22 +0300

    Functions render,changeSpeed,ChangeColor,crashFigure,changeSize

--- SVGCanvas/app.js
+++ SVGCanvas/app.js
@@ -1,71 +1,78 @@
 (function ballGame() {
-    const canvasBall = document.getElementById('canvasBall');
+    const canvas = document.getElementById('canvasBall');
     const inputColor = document.getElementById('inputColor');
-    const inputRange = document.getElementById('inputRange');
-    const ctx = canvasBall.getContext('2d');
-    const ball = {
-        color: "blue",
+    const inputRangeSize = document.getElementById('inputRangeSize');
+    const inputRangeSpeed = document.getElementById('inputRangeSpeed');
+    const inputChangeFigure = document.getElementById('inputChangeFigure');
+    const ctx = canvas.getContext('2d');
+    const figure = {
+        axisX: 60,
+        axisY: 60,
         radius: 30,
-        axisX: 30,
-        axisY: 30,
-        direction: "bottom"
+        startAngle: 0,
+        endAngle: Math.PI * 2,
+        arcDirection: true,
+        color: "blue",
+        direction: "bottom",
+        speed: 1
+  
     };
 
 
-    document.body.addEventListener('keydown', onBodyKeyDown.bind(this))
-
-
-    function onBodyKeyDown(e) {
-        if (e.keyCode == 38) {
-            ball.direction = 'top';
-        } else if (e.keyCode == 39) {
-            ball.direction = 'right';
-        } else if (e.keyCode == 40) {
-            ball.direction = 'bottom';
-        } else if (e.keyCode == 37) {
-            ball.direction = 'left';
-        }
-    }
-
     function init() {
         animate();
+
+        inputColor.addEventListener('input', changeBallColor.bind(this), false);
+        inputRangeSize.addEventListener('input', changeBallSize.bind(this), false);
+        inputRangeSpeed.addEventListener('input', changeBallSpeed.bind(this), false);
+        inputChangeFigure.addEventListener('click', changeFigure.bind(this),false);
+        document.body.addEventListener('keydown', onBodyKeyClick.bind(this));
+    }
+
+    function changeBallColor(e) {
+        figure.color = e.target.value;
+    }
+
+    function changeBallSize(e) {
+        figure.radius = +e.target.value;
+    }
+
+    function changeBallSpeed(e) {
+        figure.speed = e.target.value;
+    }
+
+    function onBodyKeyClick(e) {
+        if (e.keyCode == 38) {
+            figure.direction = 'top';
+        } else if (e.keyCode == 39) {
+            figure.direction = 'right';
+        } else if (e.keyCode == 40) {
+            figure.direction = 'bottom';
+        } else if (e.keyCode == 37) {
+            figure.direction = 'left';
+        }
+    }
+    function getRandomSettings(min, max) {
+        let rand = min + Math.random() * (max - min)
+        rand = Math.round(rand);
+
+        changeFigureSettings(rand);
+
+    }
+
+    function changeFigureSettings(rand) {
+        figure.startAngle = rand;
+        figure.arcDirection = !figure.arcDirection;
+    }
+
+    function changeFigure() {
+        getRandomSettings(1, 10)
+
+        // console.log('rand', rand)
     }
 
     function animate() {
-        setTimeout(getStart, 1);
-    }
-    function render() {
-        ctx.clearRect(0, 0, canvasBall.width, canvasBall.height);
-        renderBall();
-        checkBorders();
-        moveBall();
-    }
-
-    function renderBall() {
-        ctx.beginPath();
-        ctx.arc(ball.axisX, ball.axisY, ball.radius, 0, Math.PI * 2, true);
-        ctx.fillStyle = ball.color;
-        ctx.fill();
-        ctx.stroke();
-        ctx.closePath();
-    }
-
-    function checkBorders() {
-        console.log(ball.direction, 'start');
-
-        if (ball.radius + ball.axisX >= canvasBall.width) {
-            ball.direction = "left";
-
-        } else if (ball.axisX - ball.radius <= 0) {
-            ball.direction = "right";
-
-        } else if (ball.axisY + ball.radius >= canvasBall.height) {
-            ball.direction = 'top';
-
-        } else if (ball.axisY - ball.radius <= 0) {  //почему работает не правильно с else
-            ball.direction = 'bottom';
-        }
-
+        setTimeout(getStart, figure.speed);
     }
 
     function getStart() {
@@ -73,23 +80,60 @@
         animate();
     }
 
-    function moveBall() {
-        switch (ball.direction) {
-            case 'right':
-                ball.axisX++;
-                break;
-            case 'left':
-                ball.axisX--;
-                break;
-            case 'bottom':
-                ball.axisY++;
-                break;
-            case 'top':
-                ball.axisY--;
-                break;
-        }
-        console.log(ball.direction, 'move');
+    function render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        renderFigure();
+        checkBorders();
+        moveFigure();
     }
 
+    function renderFigure() {
+        ctx.beginPath();
+        ctx.arc(figure.axisX, figure.axisY, figure.radius, figure.startAngle, figure.endAngle, figure.arcDirection);
+        ctx.fillStyle = figure.color;
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    function checkBorders() {
+        // console.log(figure.direction, 'start');
+
+        if (figure.radius + figure.axisX >= canvas.width) {
+            figure.direction = "left";
+
+        } else if (figure.axisX - figure.radius <= 0) {
+            figure.direction = "right";
+
+        } else if (figure.axisY + figure.radius >= canvas.height) {
+            figure.direction = 'top';
+
+        } else if (figure.axisY - figure.radius <= 0) {  //почему работает не правильно с else
+            figure.direction = 'bottom';
+        }
+
+    }
+
+
+    function moveFigure() {
+        switch (figure.direction) {
+            case 'right':
+                figure.axisX++;
+                break;
+            case 'left':
+                figure.axisX--;
+                break;
+            case 'bottom':
+                figure.axisY++;
+                break;
+            case 'top':
+                figure.axisY--;
+                break;
+        }
+        // console.log(figure.direction, 'move');
+    }
+
+
     init()
+
 })();
